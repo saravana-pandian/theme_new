cb24e0743ebdb31630e58780bf1277c6ac9b735d
saravana-pandian <saravanaoff2000@gmail.com>
2021-05-20 13:36:36 +0530

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'New theme',
+    'description': 'this theme is for practice',
+    'version': '1.0',
+    'author': 'Saravana',
+    'category': 'Theme/Creative',
+
+    'depends': ['website', 'website_sale'],
+    'data': ['views/assets.xml'],
+}
